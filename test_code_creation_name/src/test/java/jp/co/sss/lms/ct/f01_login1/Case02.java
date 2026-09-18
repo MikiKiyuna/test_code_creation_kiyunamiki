fa48a6d30478ae3797c216b649ceb18af66dab4d
mkiyuna <mkiyuna@192.168.98.185>
2026-09-18 16:22:57 +0900

    add:Case01.java, Case02.java 喜友名実樹 ログイン画面への遷移テストコードとログイン認証失敗テストコードの追加
    Case01では、WebElementを利用してタイトルの値取得し、期待値通りかどうか確認するテストを作成した。
    Case02では、データベースに登録されてないユーザーIDとパスワードでログインをするときのテストを実施。WebElementを利用してエラーメッセージを取得し、期待値通りかどうか確認するテストを作成した。
    期待値は以下の通り
    Case01：ログイン
    Case02：ログイン
    * ログインに失敗しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +37,36 @@ public class Case02 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		// URLに遷移
+		goTo("http://localhost:8080/lms");
+		//画面表示待ち
+		Thread.sleep(3000);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("ログイン | LMS", webDriver.getTitle());
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		// ID入力
+		webDriver.findElement(By.cssSelector("input[type='text']")).sendKeys("StudentABC01");
+		// パスワード入力
+		webDriver.findElement(By.cssSelector("input[type='password']")).sendKeys("Test12345");
+		// ログインボタン押下
+		webDriver.findElement(By.cssSelector(".btn-primary")).click();
+		// エラー表示待ち
+		visibilityTimeout(By.cssSelector(".btn-primary"), 5);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		WebElement errorMessage = webDriver.findElement(By.cssSelector(".help-inline.error"));
+		assertEquals("* ログインに失敗しました。", errorMessage.getText());
 	}
 
 }
