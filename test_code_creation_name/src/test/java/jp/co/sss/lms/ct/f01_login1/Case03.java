8d1d4d9ffea2ff7590f058607a8faf9d4221bb65
mkiyuna <mkiyuna@192.168.98.190>
2026-09-24 13:25:00 +0900

    add:Case03,Case04 喜友名実樹　コース詳細画面とよくある質問画面への遷移テストコード追加
    Case03では、ログイン画面への遷移後タイトルの値を取得し、ログイン後にコース詳細画面遷移しタイトルの値を取得するテストを作成した。
    期待値は以下の通り。
    ログイン画面タイトル：ログイン | LMS
    コース詳細画面のタイトル：コース詳細 | LMS
    
    Case04では、ログイン画面への遷移後タイトルの値を取得し、ログイン後にコース詳細画面遷移しタイトルの値を取得する。その後、上部にある「機能」を押下し、「ヘルプ」を押下しヘルプ画面に遷移する。そして、ヘルプ画面にある「よくある質問」を押下し、よくある質問画面に画面遷移するテストを作成した。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +36,37 @@ public class Case03 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		// URLに遷移
+		goTo("http://localhost:8080/lms");
+		//画面表示待ち
+		Thread.sleep(3000);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		// ID入力
+		webDriver.findElement(By.cssSelector("input[type='text']")).sendKeys("StudentAA02");
+		// パスワード入力
+		webDriver.findElement(By.cssSelector("input[type='password']")).sendKeys("StudentAA2");
+		// ログインボタン押下
+		webDriver.findElement(By.cssSelector(".btn-primary")).click();
+		//画面遷移後のスクリーンショット
+		visibilityTimeout(By.className("active"), 5);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -34,29 +36,68 @@ public class Case04 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		// URLに遷移
+		goTo("http://localhost:8080/lms");
+		//画面表示待ち
+		Thread.sleep(3000);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("ログイン | LMS", webDriver.getTitle());
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		// ID入力
+		webDriver.findElement(By.cssSelector("input[type='text']")).sendKeys("StudentAA02");
+		// パスワード入力
+		webDriver.findElement(By.cssSelector("input[type='password']")).sendKeys("StudentAA2");
+		// ログインボタン押下
+		webDriver.findElement(By.cssSelector(".btn-primary")).click();
+		//画面遷移後のスクリーンショット
+		visibilityTimeout(By.className("active"), 5);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//「機能」プルダウンを押下
+		webDriver.findElement(By.cssSelector(".dropdown-toggle")).click();
+		//「ヘルプ」を押下
+		webDriver.findElement(By.cssSelector("a[href='/lms/help']")).click();
+		//画面遷移後のスクリーンショット
+		visibilityTimeout(By.tagName("h4"), 5);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		// 「よくある質問」を押下
+		webDriver.findElement(By.cssSelector("a[href='/lms/faq']")).click();
+		//画面遷移後のスクリーンショット
+		visibilityTimeout(By.tagName("h2"), 5);
+		getEvidence(new Object() {
+		});
+
+		//値の取得
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
 	}
 
 }
